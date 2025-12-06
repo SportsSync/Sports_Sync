7c2a4f5971d14f9dcb5fd50cbf5124e7c1f9283f
darshil-max <core.crew07@gmail.com>
2025-12-06 12:16:42 +0530

    search bar

--- user/user_home.php
+++ user/user_home.php
@@ -1,27 +1,47 @@
 <?php
 echo  '<div class="container">
     <h2>Elite Grounds</h2>
+     
+    <div class="container mt-3">
+      <div class="row justify-content-center align-items-center g-2">
 
-    <div class="row my-4 justify-content-center">
-      <div class="col-md-3">
-        <select id="locationFilter" class="form-select">
+       <!-- Search Bar -->
+        <div class="col-md-8">
+         <div class="input-group search-bar">
+           <span class="input-group-text">
+             <i class="bi bi-search"></i>
+           </span>
+             <input type="text" id="search-item" class="form-control" placeholder="Search Here">
+            <span class="input-group-text mic-btn">
+             <i class="bi bi-mic-fill"></i>
+            </span>
+          </div>
+        </div>
+
+        <!-- Dropdown 1 -->
+        <div class="col-md-2">
+         <select id="locationFilter" class="form-select">
           <option value="all">All Locations</option>
           <option value="surat">Surat</option>
           <option value="mumbai">Mumbai</option>
-        </select>
-      </div>
-      <div class="col-md-3">
-        <select id="sportFilter" class="form-select">
-          <option value="all">All Sports</option>
-          <option value="football">Football</option>
-          <option value="cricket">Cricket</option>
-          <option value="pickleball">Pickleball</option>
-        </select>
-      </div>
-    </div>
+         </select>
+        </div>
+
+        <!-- Dropdown 2 -->
+        <div class="col-md-2">
+         <select id="sportFilter" class="form-select">
+           <option value="all">All Sports</option>
+           <option value="football">Football</option>
+           <option value="cricket">Cricket</option>
+           <option value="pickleball">Pickleball</option>
+         </select>
+        </div>
+     </div>
+   </div>
+</div>
 
 
-    <div class="row">
+    <div class="row mt-5">
       <div class="col-md-4 mb-4 turf-card">
         <div class="card">
           <img src="../images/football.png" class="card-img-top" alt="Turf">
