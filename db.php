3ade4655348b54f19f5f4199715c2b57a5a288e1
SportsSync <core.crew07@gmail.com>
2026-03-21 16:43:20 +0530

    db local

--- db.php
+++ db.php
@@ -1,20 +1,21 @@
 <?php
-//$conn = mysqli_connect("localhost", "root", "root", "turf_booking_system",3307);
-
-  //  if (!$conn) {
-    //    die("Connection failed: " . mysqli_connect_error());
-    //}
-    // if($conn){
-    //     echo "connected";
-    // }
   
+// $host = "hopper.proxy.rlwy.net";
+// $port = 21377;
+// $user = "root";
+// $password = "vmnBiktDcQljzcKjbPTKEmyqGcPPxeIr";
+// $dbname = "turf_booking_system";
+//     $conn=mysqli_connect($host,$user,$password,$dbname,$port);
+//     if(!$conn)
+//     {
+//         die("Connection failed: " . mysqli_connect_error());
+//     }
+$host="localhost:3306";
+    $username="root";
+    $password="";
+    $dbname="turf_booking_system";
 
-$host = "hopper.proxy.rlwy.net";
-$port = 21377;
-$user = "root";
-$password = "vmnBiktDcQljzcKjbPTKEmyqGcPPxeIr";
-$dbname = "turf_booking_system";
-    $conn=mysqli_connect($host,$user,$password,$dbname,$port);
+    $conn=mysqli_connect($host,$username,$password,$dbname);
     if(!$conn)
     {
         die("Connection failed: " . mysqli_connect_error());
--- owner/db.php
+++ owner/db.php
@@ -1,20 +1,22 @@
 <?php
-//$conn = mysqli_connect("localhost", "root", "root", "turf_booking_system",3307);
 
-  //  if (!$conn) {
-    //    die("Connection failed: " . mysqli_connect_error());
-    //}
-    // if($conn){
-    //     echo "connected";
-    // }
-  
+// $host = "hopper.proxy.rlwy.net";
+// $port = 21377;
+// $user = "root";
+// $password = "vmnBiktDcQljzcKjbPTKEmyqGcPPxeIr";
+// $dbname = "turf_booking_system";
+//     $conn=mysqli_connect($host,$username,$password,$dbname,$port);
+//     if(!$conn)
+//     {
+//         die("Connection failed: " . mysqli_connect_error());
+//     }
 
-$host = "hopper.proxy.rlwy.net";
-$port = 21377;
-$user = "root";
-$password = "vmnBiktDcQljzcKjbPTKEmyqGcPPxeIr";
-$dbname = "turf_booking_system";
-    $conn=mysqli_connect($host,$username,$password,$dbname,$port);
+$host="localhost:3306";
+    $username="root";
+    $password="";
+    $dbname="turf_booking_system";
+
+    $conn=mysqli_connect($host,$username,$password,$dbname);
     if(!$conn)
     {
         die("Connection failed: " . mysqli_connect_error());
